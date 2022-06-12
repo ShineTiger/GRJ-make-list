da0d07c04b61bc7095f69925fa01864952dd8c7b
MiryangJung <miryang.jung@greenlabs.co.kr>
2022-06-12 15:14:18 +0900

    같은 역할을 하는 이름 다른 두 가지 함수를 합치기

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,19 +5,15 @@ import Modal from "./Modal";
 const Nav = ({data}) => {
   const [isView, setIsView] = useState(false);
 
-  const view = () => {
-    setIsView(true);
-  };
-
-  const handleBtnModal = () => {
-    setIsView(false)
-  };
+  const handleModal = (value) => {
+    setIsView(value)
+  }
   
   //and연산자의 첫번째 피연산자가 truthy면 두번째 피연산자를 반환한다!
     return(
         <nav>
-          <button onClick={view}>추가</button>
-          {isView && <Modal handleBtnModal={handleBtnModal} onCreate={onCreate} />} 
+          <button onClick={()=>handleModal(true)}>추가</button>
+          {isView && <Modal handleBtnModal={()=>handleModal(false)} onCreate={onCreate} />} 
         <ul>{data.map((nav) => (
           <li key={nav.id}>{nav.product}</li>
         ))}</ul>
